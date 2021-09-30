a29f20f8e2cd515b2bf6ae16dc3185502f425af0
Bato Balint <balint.bato@gmail.com>
2021-09-30 11:21:07 +0200

    feltolt method

---
+++ src/hu/petrik/Veletlen.java
@@ -0,0 +1,24 @@
+package hu.petrik;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public final class Veletlen {
+    private static Random random = new Random();
+    private Veletlen() {
+    }
+
+    private List<String> feltolt(String fajlnev) {
+        List<String> fajlSorai = null;
+        try {
+            fajlSorai = Files.readAllLines(Paths.get(fajlnev));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return fajlSorai;
+    }
+}
+
